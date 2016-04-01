57e3e0806655d0633c94da25413d4d5d20740df0
Paul T <paultavares1@gmail.com>
2016-04-01 18:44:50 -0400

    New DOM Utility - domAddEventListener

---
+++ src/domutils/domAddEventListener.js
@@ -0,0 +1,36 @@
+define(function () {
+    /**
+     * Adds an event handler to a DOM element and returns back an
+     * object that allows for removal of the event.
+     *
+     * @function domAddEventListener
+     *
+     * @param {HTMLElement} el
+     * @param {String} event
+     * @param {Function} callback
+     * @param {Boolean} [capture]
+     *
+     * @return DOMEventListener
+     *
+     * @example
+     *
+     * var listener = domAddEventHandler(myEle, "click", function(){});
+     * ...
+     * listener.remove();
+     */
+    return function (el, event, callback, capture) {
+        ele.addEventListener(event, callback, capture);
+        /**
+         * A DOM Event listener.
+         *
+         * @typedef DOMEventListener
+         *
+         * @property {Function} remove
+         */
+        return Object.create({
+            remove: function(){
+                ele.removeEventListener(event, callback, capture);
+            }
+        });
+    };
+});
