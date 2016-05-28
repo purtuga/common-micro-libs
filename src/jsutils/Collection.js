14366bd7c0d2ea4cf4463fe96741da31922da901
Paul T <paultavares1@gmail.com>
2016-05-28 10:55:18 -0400

    Collection Class
    - Added Collection Constructor with support for events

---
+++ src/jsutils/Collection.js
@@ -0,0 +1,90 @@
+define([
+    "./Compose",
+    "./dataStore",
+    "./EventEmitter"
+], function(
+    Compose,
+    dataStore,
+    EventEmitter
+){
+
+    var
+    PRIVATE         = dataStore.create(),
+    ArrayPrototype  = Array.prototype,
+    changeMethods   = [
+        'pop',
+        'push',
+        'shift',
+        'splice',
+        'unshift'
+    ],
+
+    /**
+     * An Array like object with the added ability to listen to events
+     *
+     * @class Collection
+     *
+     * @extends Compose
+     * @extends EventEmitter
+     * @extends Array
+     *
+     * @fires Collection#change
+     */
+    Collection = /** @lends Collection.prototype */{
+        init: function(initialValues){
+            var inst = {
+                data: Array.isArray(initialValues) ? initialValues : []
+            };
+
+            PRIVATE.set(this, inst);
+        },
+
+        /**
+         * The size of the collection.
+         *
+         * @protected
+         *
+         * @type {Number}
+         */
+        length: 0,
+
+        /**
+         * Returns the size of the collection
+         *
+         * @returns {Number}
+         */
+        size: function(){
+            return PRIVATE.get(this).data.length;
+        }
+    };
+
+    // Add all methods of Array.prototype to the collection
+    Object.keys(Array.prototype).forEach(function(method){
+        if (method === "constructor" || typeof ArrayPrototype[method] !== "function") {
+            return;
+        }
+
+        var doEvents = changeMethods.indexOf(method) > -1;
+
+        Collection[method] = function(){
+            var inst     = PRIVATE.get(this),
+                response = ArrayPrototype[method].apply(inst.data, arguments);
+
+            // If Array method can manipulate the array, then emit event
+            if (doEvents) {
+                this.length = inst.data.length;
+
+                /**
+                 * Collection was changed.
+                 *
+                 * @event Collection#change
+                 */
+                this.emit("change", response);
+            }
+
+            return response;
+        }
+    });
+
+    return Compose.extend(EventEmitter, Collection);
+});
