5bf2dd156e71d32502a76eebd7a24a80944ab70e
Paul T <paultavares1@gmail.com>
2016-10-09 18:27:26 -0400

    Enh. to parseXML

--- src/jsutils/parseXML.js
+++ src/jsutils/parseXML.js
@@ -12,19 +12,27 @@
 var parseXML = function(data){
     var xml;
 
-    if ( !data || typeof data !== "string" ) {
+    if (!data || typeof data !== "string") {
         return null;
     }
 
     // Support: IE9
     try {
-        xml = ( new window.DOMParser() ).parseFromString( data, "text/xml" );
-    } catch ( e ) {
+        xml = (new window.DOMParser()).parseFromString(data, "text/xml");
+    } catch (e) {
         xml = undefined;
     }
 
-    if ( !xml || xml.getElementsByTagName( "parsererror" ).length ) {
-        throw new Error( "Invalid XML: " + data );
+    var errors;
+    if (!xml || (errors = xml.getElementsByTagName("parsererror")).length) {
+        var err;
+        try {
+            err = new Error( "Invalid XML: " + errors[0].textContent);
+            err.xmlData = data;
+        } catch (e) {
+            err = new Error("Invalid XML: " + data);
+        }
+        throw err;
     }
 
     return xml;
