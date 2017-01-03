1980a9af155a06fd648e10ecd845436b749c98e3
Paul T <paultavares1@gmail.com>
2017-01-03 18:47:43 -0500

    Added fetchCheckForHttpErrors - to use with fetch() api

---
+++ src/jsutils/fetchCheckForHttpErrors.js
@@ -0,0 +1,19 @@
+/**
+ * Checks the HTTP `Response` Object of a `fetch` to see if there was an HTTP error.
+ *
+ * @param {Response} response
+ *
+ * @returns {Response|Promise.reject}
+ *  IN case of a Promise.reject, the Error provided will have a property
+ *  called `response` that holds the `Response` instance
+ */
+export default function fetchCheckForHttpErrors(response) {
+    // If server returned an error code, then reject promise
+    if (response.ok) {
+        return response;
+    }
+
+    var error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+}
