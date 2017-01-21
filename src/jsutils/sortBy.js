fed29a8837445228ab0f00f0e2368374e5d9a458
Paul T <paultavares1@gmail.com>
2017-01-21 12:00:52 -0500

    sortBy doc update

--- src/jsutils/sortBy.js
+++ src/jsutils/sortBy.js
@@ -1,12 +1,28 @@
 /**
  * Sorts an Array/Collection of Objects by a given attribute. Sort is done in place
  *
- * @param {Array|Collection} arr
+ * @param {Array<Object>|Collection<Object>} arr
  * @param {String} attr
  * @param {String} [dir="asc"]
  *  Sorting direction. Possible values:
  *  -   `asc`: Ascending (Default)
  *  -   `desc`: Descending
+ *
+ * @example
+ *
+ * let arr = [
+ *      {
+ *          title: "title 5"
+ *      },
+ *      {
+ *          title: "title 9"
+ *      },
+ *      {
+ *          title: "title 1"
+ *      }
+ * ];
+ *
+ * sortBy(arr, "title");
  */
 export default function sortBy(arr, attr, dir = "asc"){
     if (!Array.isArray(arr) || !attr) {
