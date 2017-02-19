05f5eede7a6f4af2adaf03b8ae63eace74382237
Paul T <paultavares1@gmail.com>
2017-02-19 15:45:35 -0500

    Added ArrayFindBy and objectPick utilities

---
+++ src/jsutils/arrayFindBy.js
@@ -0,0 +1,31 @@
+/**
+ * Finds an element in the array by executing an iterator callback. Can
+ * return either the found element (default) or its index.
+ *
+ * @param {Array} arr
+ * @param {Function|Object} iterator
+ * @param {Boolean} returnIndex
+ * @returns {*}
+ */
+export default function arrayFindBy(arr, iterator, returnIndex) {
+    let response;
+
+    if (returnIndex) {
+        response = -1;
+    }
+
+    arr.some((arrayItem, index) => {
+        if (iterator(arrayItem, index)) {
+            if (returnIndex) {
+                response = index;
+
+            } else {
+                response = arrayItem;
+            }
+
+            return true;
+        }
+    });
+
+    return response;
+}
---
+++ src/jsutils/objectPick.js
@@ -0,0 +1,29 @@
+/**
+ * Given an object and a set of properties, this method will return a new
+ * object with only the requested properties.
+ *
+ * @param {Object} obj
+ * @param {...String|String[]} props
+ *
+ * @returns {Object}
+ */
+export default function objectPick(obj,...props) {
+    // Yes, I could have used lodash.pick... but after I looked at its source,
+    // the code seemed like it covers many more robust uses cases...I just need
+    // to pick properties from a plain object.
+
+    if (!obj){
+        return {};
+    }
+
+    if (Array.isArray(props[0])) {
+        props = props[0];
+    }
+
+    return props.reduce((result, prop) => {
+        if (obj.hasOwnProperty(prop)) {
+            result[prop] = obj[prop];
+        }
+        return result;
+    }, {});
+}
