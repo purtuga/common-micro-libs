3a695a9445b673889a6602ca967de14f82d42c6c
Paul T <paultavares1@gmail.com>
2017-05-07 13:18:22 -0400

    DomResizableInteraction - new utility to make elements resizable + enh. to domAddEventListener

---
+++ src/domutils/DomResizableInteraction/index.js
@@ -0,0 +1,207 @@
+import EventEmitter         from "../../jsutils/EventEmitter"
+import objectExtend         from "../../jsutils/objectExtend"
+import dataStore            from "../../jsutils/dataStore"
+import domAddEventListener  from "../domAddEventListener"
+
+//========================================================================
+const PRIVATE   = dataStore.create();
+const WINDOW    = window;
+const EV_RESIZE = "resize";
+const EV_START  = `${ EV_RESIZE }-start`;
+const EV_END    = `${ EV_RESIZE }-end`;
+
+/**
+ * Utility to make a given DOM element resizable by dragging its
+ * edges.
+ *
+ * @class DomResizableInteraction
+ * @extends EventEmitter
+ *
+ * @param {Object} options
+ * @param {HTMLElement} options.ele
+ * @param {Boolean|String|HTMLElement} [options.seHandle=true]
+ *  The Bottom-Right drag handle. Can be:
+ *
+ *  -   `Boolean`: indicating if handle should be used or not. In such case,
+ *      this utility will insert its own markup to represent the handle.
+ *  -   `String`: a selector identifing the drag handle (selector is applied
+*       to `options.ele`)
+ *  -   `HTMLElement`: An HTML element
+ *
+ * @fires DomResizableInteraction#resize-start
+ * @fires DomResizableInteraction#resize-end
+ * @fires DomResizableInteraction#resize
+ */
+const DomResizableInteraction = EventEmitter.extend(/** @lends DomResizableInteraction.prototype */{
+    init(options) {
+        if (PRIVATE.get(this)) {
+            return;
+        }
+
+        let inst = {
+            opt:  objectExtend({}, this.getFactory().defaults, options)
+        };
+
+        PRIVATE.set(this, inst);
+        setupHandles.call(this);
+
+        this.onDestroy(() => {
+            // Destroy all Compose object
+            Object.keys(inst).forEach(function (prop) {
+                if (inst[prop]) {
+                    [
+                        "destroy",      // Compose
+                        "remove",       // DOM Events Listeners
+                        "off"           // EventEmitter Listeners
+                    ].some((method) => {
+                        if (inst[prop][method]) {
+                            inst[prop][method]();
+                            return true;
+                        }
+                    });
+
+                    inst[prop] = undefined;
+                }
+            });
+
+            PRIVATE['delete'](this);
+        });
+    }
+});
+
+function setupHandles() {
+    let inst        = PRIVATE.get(this);
+    // let {seHandle}  = inst.opt;
+
+    // FIXME: support for Boolean and selector definitions for handles
+
+    inst.seHandleEle = inst.opt.seHandle;
+
+    inst.handleEvents = addEventHandlingToHandle.call(this, {
+        handle: inst.seHandleEle,
+        movement: "all"
+    });
+}
+
+/**
+ * Adds event handling to a give handle.
+ *
+ * @param {Object} options
+ * @param {HTMLElement} options.handle
+ * @param {Object} [options.movement="all"]
+ *  The type of movement the handle allows. Valid values are:
+ *
+ *  -   `all`: All directions (north-south/east-west)
+ *  -   `ns`: vertically only (north south)
+ *  -   `ew`: Horizontally only (east west)
+ *
+ * @return {Object}
+ *  An object containing a `.remove()` method that can be used to
+ *  destruct all event handlers
+ */
+function addEventHandlingToHandle(options = {movement: "all"}) {
+    let inst        = PRIVATE.get(this);
+    let domEvents   = {};
+    let emit        = this.emit.bind(this);
+    let {
+        ele:resizableEle,
+        minWidth,
+        minHeight   }   = inst.opt;
+    let {
+        handle,
+        movement = "all" }  = options;
+    let boxWidth;
+    let boxHeight;
+    let handleX;
+    let handleY;
+    let resizeWhenMouseMoves = handleEle => {
+        let newWidth    = boxWidth + (handleEle.clientX - handleX);
+        let newHeight   = boxHeight + (handleEle.clientY - handleY);
+        let fireEvent   = false;
+
+        if (newWidth >= minWidth && (movement === "all" || movement === "ew")) {
+            resizableEle.style.width = newWidth + "px";
+            fireEvent = true;
+        }
+
+        if (newHeight >= minHeight && (movement === "all" || movement === "ns")) {
+            resizableEle.style.height = newHeight + "px";
+            fireEvent = true;
+        }
+
+        if (fireEvent) {
+            /**
+             * Element was resized
+             *
+             * @event DomResizableInteraction#resize
+             */
+            emit(EV_RESIZE);
+        }
+    };
+    let stopResizing = () => {
+        let fireEvent = false;
+
+        if (domEvents.mousemove) {
+            domEvents.mousemove.remove();
+            domEvents.mousemove = null;
+            fireEvent = true;
+        }
+
+        if (domEvents.mouseup) {
+            domEvents.mouseup.remove();
+            domEvents.mouseup = null;
+            fireEvent = true;
+        }
+
+        if (fireEvent) {
+            /**
+             * Resizing of element has ended (user released the mouse (mouseup).
+             *
+             * @event DomResizableInteraction#resize-end
+             */
+            emit(EV_END);
+        }
+    };
+
+    domEvents.mousedown = domAddEventListener(handle, "mousedown touchstart", handleEle => {
+        boxWidth    = resizableEle.clientWidth;
+        boxHeight   = resizableEle.clientHeight;
+        handleX     = handleEle.clientX;
+        handleY     = handleEle.clientY;
+
+        domEvents.mousemove = domAddEventListener(WINDOW, "mousemove touchmove", resizeWhenMouseMoves, false);
+        domEvents.mouseup = domAddEventListener(WINDOW, "mouseup touchend", stopResizing, false);
+
+        /**
+         * Resizing of element is about to start (user moused down on handle)
+         *
+         * @event DomResizableInteraction#resize-start
+         */
+        emit(EV_START);
+    }, false);
+
+    return Object.create({
+        remove() {
+            Object.keys(domEvents).forEach(evName => {
+                if (domEvents[evName]) {
+                    domEvents[evName].remove();
+                    domEvents[evName] = null;
+                }
+            });
+        }
+    });
+}
+
+
+DomResizableInteraction.defaults = {
+    ele:        null,
+    minWidth:   50,
+    minHeight:  50,
+    // drag handles
+    seHandle:   true
+};
+
+
+export default DomResizableInteraction;
+
+
--- src/domutils/domAddEventListener.js
+++ src/domutils/domAddEventListener.js
@@ -6,6 +6,8 @@
  *
  * @param {HTMLElement} ele
  * @param {String} event
+ *  The event to listen to (ex. `click`). Multiple events can be defined
+ *  by separating them with whitespace
  * @param {Function} callback
  * @param {Boolean} [capture]
  *
@@ -18,17 +20,30 @@
  * listener.remove();
  */
 export default function domAddEventListener(ele, event, callback, capture) {
-    ele.addEventListener(event, callback, capture);
+    let events      = event.split(/\s+/);
+    let evListeners = {};
+
+    events.forEach(evName => {
+        ele.addEventListener(evName, callback, capture);
+        evListeners[evName] = {
+            remove: () => ele.removeEventListener(evName, callback, capture)
+        }
+    });
+
     /**
      * A DOM Event listener.
      *
-     * @typedef DOMEventListener
+     * @typedef {Object} DOMEventListener
      *
      * @property {Function} remove
+     * @property {Object} listeners
+     *  List of listeners that were bound to the DOM element. Each listeners has a
+     *  corresponding `.remove()` method.
      */
     return Object.create({
+        listeners: evListeners,
         remove: function () {
-            ele.removeEventListener(event, callback, capture);
+            events.forEach(evName => evListeners[evName].remove());
         }
     });
 }
