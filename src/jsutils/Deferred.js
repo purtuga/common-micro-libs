e82bead15bfff3492668f3b0e4f833353b06f33b
Paul T <paultavares1@gmail.com>
2017-06-10 12:03:30 -0400

    New Deferred utility

---
+++ src/jsutils/Deferred.js
@@ -0,0 +1,44 @@
+import Promise from "./es6-promise"
+
+export default Object.create({
+    /**
+     * Creates a `Deferred` object which is essentially an un-resolved promise
+     * that, in addition to the normal Promise interface, also exposes a `resolve`
+     * and `reject` method - thus allowing control over the promise from the "outside".
+     *
+     * @return {DeferredPromise}
+     *
+     * @example
+     *
+     * let dfd = Deferred.create();
+     * addToQueue(dfd);
+     * return dfd.promise;
+     *
+     * //... later
+     * queue[0].resolve({msg: "done"});
+     */
+    create() {
+        let resolvedDeferred;
+        let rejectDeferred;
+        /**
+         * A Deferred object. A promise that exposes 3 additional attributes
+         *
+         * @typedef {Promise} DeferredPromise
+         *
+         * @property {Function} then
+         * @property {Promise} promise
+         * @property {Function} resolve
+         * @property {Function} reject
+         */
+        let deferred = new Promise((resolve, reject) => {
+            resolvedDeferred    = resolve;
+            rejectDeferred      = reject;
+        });
+
+        deferred.promise    = deferred;
+        deferred.resolve    = resolvedDeferred;
+        deferred.reject     = rejectDeferred;
+
+        return deferred;
+    }
+});
