eb8f21f4a019c8497b90f7828e42ea25c71a6ca3
Paul T <paultavares1@gmail.com>
2017-09-21 20:16:33 -0400

    ObservableObject - Support for Computed properties (initial)

--- src/jsutils/ObservableObject.js
+++ src/jsutils/ObservableObject.js
@@ -2,13 +2,16 @@ import Compose          from "./Compose"
 import objectExtend     from "./objectExtend"
 import dataStore        from "./dataStore"
 import EventEmitter     from "./EventEmitter"
+import nextTick         from "./nextTick"
 
 //=======================================================
 const PRIVATE                 = dataStore.create();
-const GLOBAL_NOTIFY_DELAY     = 10;
 
+// aliases
 const objectDefineProperty    = Object.defineProperty;
 const objectHasOwnProperty    = Object.prototype.hasOwnProperty;
+
+let dependeeList = [];
 
 /**
  * Adds the ability to observe `Object` property values for changes.
@@ -52,10 +55,17 @@ const objectHasOwnProperty    = Object.prototype.hasOwnProperty;
  * });
  *
  */
-let ObservableObject = /** @lends ObservableObject.prototype */{
-    init(model) {
+let ObservableObject = Compose.extend(/** @lends ObservableObject.prototype */{
+    init(model, options) {
         if (model) {
+            // FIXME: need to create prop with original getter/setters - or no?
             objectExtend(this, model);
+        }
+
+        const opt = objectExtend({}, this.getFactory().defaults, options);
+
+        if (opt.watchAll) {
+            Object.keys(this).forEach(propName => makePropWatchable(this, propName));
         }
     },
 
@@ -73,7 +83,7 @@ let ObservableObject = /** @lends ObservableObject.prototype */{
      */
     on: function(prop, callback){
         if (objectHasOwnProperty.call(this, prop)) {
-            watchProp.call(this, prop);
+            makePropWatchable(this, prop);
             return getInstance.call(this).on(prop, callback);
         }
     },
@@ -105,7 +115,7 @@ let ObservableObject = /** @lends ObservableObject.prototype */{
      */
     once: function(prop, callback){
         if (objectHasOwnProperty.call(this, prop)) {
-            watchProp.call(this, prop);
+            makePropWatchable(this, prop);
             return getInstance.call(this).once(prop, callback);
         }
     },
@@ -123,7 +133,7 @@ let ObservableObject = /** @lends ObservableObject.prototype */{
             watched[prop].notify();
         }
     }
-};
+});
 
 /**
  * Returns the private Instance data for this object
@@ -156,100 +166,179 @@ function getInstance(){
  * and if not, it sets one up for it.
  *
  * @private
- * @this ObservableObject
+ * @param {ObservableObject} observable
+ * @param {String} propName
+ * @param {Function} [valueGetter]
+ * @param {Function} [valueSetter]
  *
- * @param {String} prop
  */
-function watchProp(prop){
-    let observable      = this;
+function makePropWatchable(observable, propName, valueGetter, valueSetter){
     let inst            = getInstance.call(observable);
     let watched         = inst.watched;
-    let eventRunning    = false;
-    let currentValue, propDescriptor, priorGetter, priorSetter;
+    let currentValue, propDescriptor;
 
-    if (!watched[prop]){
-        propDescriptor = Object.getOwnPropertyDescriptor(observable, prop);
+    if (watched[propName]){
+        return;
+    }
 
-        if (propDescriptor) {
-            if (propDescriptor.configurable === false) {
-                return;
+    propDescriptor = Object.getOwnPropertyDescriptor(observable, propName);
+
+    if (propDescriptor) {
+        if (propDescriptor.configurable === false) {
+            // TODO: should we throw()?
+            return;
+        }
+
+        valueGetter = valueGetter || propDescriptor.get;
+        valueSetter = valueSetter || propDescriptor.set;
+
+        if (!valueGetter) {
+            currentValue = propDescriptor.value;
+        }
+    }
+
+    // if we're able to remove the current property (ex. Constants would fail),
+    // then change this attribute to be watched
+    if (delete observable[propName]) {
+        watched[propName] = {
+            dependees: [],
+            oldVal: currentValue,
+            newVal: currentValue,
+            queued: false,
+            notify: function(delay){
+                // Queue the notification if not yet done
+                if (watched[propName].queued) {
+                    return;
+                }
+
+                watched[propName].queued = true;
+
+                const callListeners = function(){
+                    watched[propName].queued = false;
+                    inst.emit(propName, watched[propName].newVal, watched[propName].oldVal);
+                    watched[propName].dependees.forEach(cb => cb.call(observable));
+                };
+
+                if (!delay) {
+                    callListeners();
+                    return;
+                }
+
+                nextTick(() => callListeners());
             }
-            priorGetter = propDescriptor.get;
-            priorSetter = propDescriptor.set;
-        }
+        };
 
-        currentValue = this[prop];
+        objectDefineProperty(observable, propName, {
+            enumerable:     true,
+            configurable:   true,
 
-        // if we're able to remove the current property (ex. Constants would fail),
-        // then change this attribute to be watched
-        if (delete this[prop]) {
-            watched[prop] = {
-                oldVal: currentValue,
-                newVal: currentValue,
-                queued: null,
-                notify: function(delay){
-                    // Trigger event, but only if this update was not a
-                    // result of an earlier event trigger to this same prop.
-                    if (eventRunning) {
-                        return;
-                    }
-
-                    eventRunning = true;
-
-                    var callListeners = function(){
-                        inst.emit(prop, watched[prop].newVal, watched[prop].oldVal);
-                        watched[prop].queued = null;
-                        eventRunning = false;
-                    };
-
-                    if (delay === undefined) {
-                        callListeners();
-                        return;
-                    }
-
-                    watched[prop].queued = setTimeout(callListeners, GLOBAL_NOTIFY_DELAY);
+            // Getter will either delegate to the prior getter(),
+            // or return the value that was originally assigned to the property
+            get: function(){
+                // If there is a dependee listening for changes right now, then
+                // store it for future notification
+                if (dependeeList && dependeeList.length) {
+                    dependeeList.forEach(dependeeCallback => {
+                        if (watched[propName].dependees.indexOf(dependeeCallback) === -1) {
+                            watched[propName].dependees.push(dependeeCallback);
+                        }
+                    });
                 }
-            };
 
-            currentValue = undefined;
+                return valueGetter ? valueGetter() : watched[propName].newVal;
+            },
 
-            objectDefineProperty(this, prop, {
-                enumerable:     true,
-                configurable:   true,
+            // Setter is how we detect changes to the value.
+            set: function(newValue){
+                let oldValue = valueGetter ? valueGetter() : watched[propName].newVal;
 
-                get: function(){
-                    return priorGetter ? priorGetter() : watched[prop].newVal;
-                },
+                if (valueSetter) {
+                    valueSetter.call(observable, newValue);
 
-                set: function(newValue){
-                    let oldValue = priorGetter ? priorGetter() : watched[prop].newVal;
-
-                    if (priorSetter) {
-                        priorSetter.call(observable, newValue);
-
-                    } else {
-                        watched[prop].oldVal = oldValue;
-                        watched[prop].newVal = newValue;
-                    }
-
-                    // Dirty checking...
-                    // Only trigger if values are different. Also, only add a trigger
-                    // if one is not already queued.
-                    if (!watched[prop].queued && newValue !== oldValue) {
-                        watched[prop].notify(20);
-                    }
+                } else {
+                    watched[propName].oldVal = oldValue;
+                    watched[propName].newVal = newValue;
                 }
-            });
 
-        } else {
-            try {
-                console.log("ObservableObject(Error) Unable to watch property [" + prop + "]");
-            } catch(e){}
-        }
+                // Dirty checking...
+                // Only trigger if values are different. Also, only add a trigger
+                // if one is not already queued.
+                if (!watched[propName].queued && newValue !== oldValue) {
+                    watched[propName].notify(true);
+                }
+            }
+        });
+
+    } else {
+        try {
+            console.log("ObservableObject(Error) Unable to watch property [" + propName + "] - delete failed");
+        } catch(e){}
     }
 }
 
-ObservableObject = Compose.extend(ObservableObject);
+/**
+ * Created a computed property on a given object
+ *
+ * @method Observable.createComputed
+ *
+ * @param {Object} obj
+ * @param {String} propName
+ * @param {Function} valueGenerator
+ */
+function createComputed(obj, propName, valueGenerator) {
+    if (obj && propName && valueGenerator) {
+        let runValueGenerator = true;
+        let value;
+        let dependencyChangeNotifier = () => {
+            value = null;
+            runValueGenerator = true;
+        };
+        const addToDependeeList = () => {
+            if (dependeeList.indexOf(addToDependeeList) === -1) {
+                dependeeList.push(dependencyChangeNotifier);
+            }
+        };
+        const removeFromDependeeList = () => {
+            let index = dependeeList.indexOf(addToDependeeList);
+            if (index !== -1) {
+                dependeeList.splice(index, 1);
+            }
+        };
+
+        makePropWatchable(obj, propName,
+            // Getter
+            () => {
+                if (!runValueGenerator) {
+                    return value;
+                }
+
+                runValueGenerator = false;
+                addToDependeeList();
+
+                try {
+                    value = valueGenerator.call(obj);
+                }
+                catch(e) {
+                    removeFromDependeeList();
+                    throw e;
+                }
+
+                removeFromDependeeList();
+
+                return value
+            },
+
+            // Setter
+            () => {
+                // FIXME: should we output to console?
+            }
+        );
+    }
+}
+
+
+ObservableObject.createComputed = createComputed;
+
 
 /**
  * Adds ObservableObject capabilities to an object.
@@ -274,6 +363,10 @@ ObservableObject.mixin = function(obj){
         });
     }
     return obj;
+};
+
+ObservableObject.defaults = {
+    watchAll: true
 };
 
 export default ObservableObject;
--- test/observableObject.js
+++ test/observableObject.js
@@ -1,10 +1,13 @@
 require                 = require('@std/esm')(module, { cjs: true, esm: 'js' });
 const test              = require("tape");
 const ObservableObject  = require("../src/jsutils/ObservableObject").default;
+const delay             = (ms) => new Promise(resolve => setTimeout(resolve, ms || 2));
 
 test("ObservableObject", t => {
     t.equal(typeof ObservableObject, "function", "is defined");
     t.equal(typeof ObservableObject.create, "function", "has .create() method");
+    t.equal(typeof ObservableObject.mixin, "function", "has .mixin() method");
+    t.equal(typeof ObservableObject.createComputed, "function", "has .createComputed() method");
 
     t.test("Instance Methods", st => {
         const model = ObservableObject.create({name: "paul"});
@@ -23,16 +26,92 @@ test("ObservableObject", t => {
     });
 
     t.test("Events", st => {
-        st.plan(2);
+        st.plan(3);
 
         const model = ObservableObject.create({name: "paul"});
 
         model.once("name", () => {
             st.ok(true, "change event triggered");
         });
-        st.equal(model.name, "paul", "has original value after watch");
+
+        st.equal(model.name, "paul", "has original value after calling .on()");
         model.name = "paul 1";
+
+        const changeCallback = () => changeCallback.count++;
+        changeCallback.count = 0;
+
+        model.on("name", changeCallback);
+        [1, 2, 3].forEach(n => model.name = `paul-${n}`);
+
+        setTimeout(() => {
+            st.equal(changeCallback.count, 1, "Callbacks are executed only once per event loop");
+        }, 40);
     });
+
+    t.test("Supports existing getters and setters", st => {
+        st.plan(4);
+
+        let obj = ObservableObject.create();
+        let value = "paul";
+        let getter = () => {
+            getter.count++;
+            return value;
+        };
+        let setter = (val) => {
+            setter.count++;
+            return value = val;
+        };
+
+        getter.count = 0;
+        setter.count = 0;
+
+        Object.defineProperty(obj, "name", {
+            configurable: true,
+            get: getter,
+            set: setter
+        });
+
+        obj.once("name", () => {
+            st.equal(setter.count, 1, "Original Setter is called");
+        });
+
+        st.equal(getter.count, 0, "getter has not been called");
+        st.equal(obj.name, "paul", "getter return original value");
+        st.equal(getter.count, 1, "Original Getter is called");
+
+        obj.name = "paul1";
+    });
+
+    t.test("Computed properties", st => {
+        st.plan(3);
+
+        let obj = ObservableObject.create({
+            firstName: "Paul",
+            lastName: "Tavares"
+        });
+
+        ObservableObject.createComputed(obj, "fullName", function() {
+            return `${ this.firstName } ${ this.lastName }`;
+        });
+
+        st.equal(obj.fullName, "Paul Tavares", "creates Computed value");
+
+        obj.firstName = "Paul1";
+        delay()
+            .then(() => {
+                st.equal(obj.fullName, "Paul1 Tavares", "change to dependency prop updates Computed value")
+
+                obj.firstName = "john";
+                obj.lastName = "smith";
+
+                return delay();
+            })
+            .then(() => {
+                st.equal(obj.fullName, "john smith", "change to multiple depdencies propse updates computed");
+            })
+            .catch(e => console.log(e));
+    });
+
 
     t.end();
 });
