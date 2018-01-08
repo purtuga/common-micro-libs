dbd42b6e6fcdd934a01f855cd69c59839cd6e470
Paul T <paultavares1@gmail.com>
2018-01-07 19:20:12 -0500

    Updates to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require("path");
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const packageJson = require("./package.json");
+const path              = require("path");
+const UglifyJSPlugin    = require('uglifyjs-webpack-plugin');
+const packageJson       = require("./package.json");
+const isProduction      = process.env.NODE_ENV === "production";
 
-module.exports = {
+const config = module.exports = {
     entry: "./src/index.js",
     output: {
-        library: "commonMicroLibs",
-        libraryTarget: "umd",
-        filename: `${ packageJson.name }.js`,
-        path: path.join(__dirname, "dist")
+        library:        "commonMicroLibs",
+        libraryTarget:  "umd",
+        filename:       `${ packageJson.name }.js`,
+        path:           path.join(__dirname, "dist")
     },
     module: {
         rules: [
@@ -53,22 +54,51 @@ module.exports = {
         openPage: "dev.html"
     },
     plugins: [
-        // new UglifyJSPlugin({
-        //     beautify: true,
-        //     mangle: false,
-        //     compress: {
-        //         warnings: false,
-        //         collapse_vars: false,
-        //         sequences: false,
-        //         // conditionals: false,
-        //         comparisons: false,
-        //         booleans: false,
-        //         //evaluate:       false,
-        //         hoist_funs: false,
-        //         join_vars: false,
-        //         if_return: false,
-        //         cascade: false
-        //     }
-        // })
+        new UglifyJSPlugin({
+            beautify: true,
+            mangle: false,
+            compress: {
+                warnings: false,
+                collapse_vars: false,
+                sequences: false,
+                // conditionals: false,
+                comparisons: false,
+                booleans: false,
+                //evaluate:       false,
+                hoist_funs: false,
+                join_vars: false,
+                if_return: false,
+                cascade: false
+            }
+        })
     ]
 };
+
+//=====================================================================
+// DEV MODE:
+//=====================================================================
+if (!isProduction) {
+    // No minify
+    config.plugins.shift();
+
+    // Set babel to target only latest browsers
+    config.module.rules.some(rule => {
+        if (rule.loader === "babel-loader") {
+            rule.options = {
+                babelrc: false,
+                presets: [
+                    ["env", {
+                        "targets": {
+                            "browsers": ["last 2 Chrome versions"]
+                        },
+                        "modules": false,
+                        "loose": true
+                    }]
+                ]
+            };
+
+            return true;
+        }
+    });
+
+}
