5f47db31522c996ee7ffa44fac46f2b735f417a7
Paul T <paultavares1@gmail.com>
2018-02-18 11:29:19 -0500

    Additional runtime aleases

--- src/jsutils/runtime-aliases.js
+++ src/jsutils/runtime-aliases.js
@@ -13,6 +13,11 @@ export const objectKeys               = Object.keys;
 
 // Array
 const arr = [];
+export const isArray        = Array.isArray;
 export const arrayForEach   = functionBindCall(arr.forEach);
 export const arrayIndexOf   = functionBindCall(arr.indexOf);
 export const arraySplice    = functionBindCall(arr.splice);
+
+// Logging
+export const consoleLog = console.log;
+export const consoleError = console.error || consoleLog;
