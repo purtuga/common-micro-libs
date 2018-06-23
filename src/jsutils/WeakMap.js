0437921c531728c04b5b1a43f723d0f2da185c53
Paul T <paultavares1@gmail.com>
2018-06-23 17:01:35 -0400

    dataStore refactored to split out WeakMap polyfill

---
+++ src/jsutils/WeakMap.js
@@ -0,0 +1,59 @@
+// POLYFILL FOR WeakMap
+//  [pt] changed how "delete" is defined so that it can work in IE8
+
+/* jshint ignore:start */
+/**
+ * @license
+ * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+var FakeWeakMap;
+if (typeof WeakMap === 'undefined') {
+    (function() {
+        var defineProperty = Object.defineProperty;
+        var counter = Date.now() % 1e9;
+
+        var WeakMap = FakeWeakMap = function() {
+            this.name = '__st' + (Math.random() * 1e9 >>> 0) + (counter++ + '__');
+        };
+
+        WeakMap.prototype = {
+            set: function(key, value) {
+                var entry = key[this.name];
+                if (entry && entry[0] === key)
+                    entry[1] = value;
+                else
+                    defineProperty(key, this.name, {value: [key, value], writable: true});
+                return this;
+            },
+            get: function(key) {
+                var entry;
+                return (entry = key[this.name]) && entry[0] === key ?
+                    entry[1] : undefined;
+            },
+            // [pt] Quotes around the delete property needed for IE8
+            "delete": function(key) {
+                var entry = key[this.name];
+                if (!entry || entry[0] !== key) return false;
+                entry[0] = entry[1] = undefined;
+                return true;
+            },
+            has: function(key) {
+                var entry = key[this.name];
+                if (!entry) return false;
+                return entry[0] === key;
+            }
+        };
+
+        window.WeakMap = WeakMap;
+    })();
+} else {
+    FakeWeakMap = WeakMap
+}
+/* jshint ignore:end */
+export { FakeWeakMap as WeakMap };
+export default FakeWeakMap;
