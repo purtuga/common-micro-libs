e6b17bd1d11802537ee62fe7e5994c7f9c50aaf3
Paul T <paultavares1@gmail.com>
2018-11-18 17:47:30 -0500

    Added BaseClass (to replace existing Compose)

---
+++ src/jsutils/BaseClass.js
@@ -0,0 +1,44 @@
+import {consoleError} from "./runtime-aliases.js";
+
+//======================================================================
+const PRIVATE = new WeakMap();
+
+/**
+ * A base class
+ */
+export class BaseClass {
+    constructor(){
+        if (this.init) { // for backwards compatibility
+            this.init(...arguments);
+        }
+    }
+
+    /**
+     * Add a callback to be exeucted when `.destroy()` is executed
+     *
+     * @param {FUnction} callback
+     */
+    onDestroy(callback) {
+        if (!PRIVATE.has(this)) {
+            PRIVATE.set(this, new Set());
+        }
+        PRIVATE.get(this).add(callback);
+    }
+
+    /**
+     * Run destroy logic, which by default is to run all callbacks stored via `.onDestroy()`
+     */
+    destroy() {
+        const callbackList = PRIVATE.get(this)
+        if (callbackList) {
+            callbackList.forEach(callback => {
+                try {
+                    callback();
+                } catch (e) {
+                    consoleError(e);
+                }
+            });
+            callbackList.clear();
+        }
+    }
+}
